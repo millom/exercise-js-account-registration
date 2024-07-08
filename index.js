712410859dc6b7506e178d8cdad60e813bb1c920
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-07-09 00:42:01 +0200

    Working border around item when error

--- index.js
+++ index.js
@@ -13,38 +13,103 @@ let isEmailDirty = false;
 let isPasswordDirty = false;
 let isConfirmDirty = false;
 
-nameTag.onkeyup = () => {
+nameTag.onkeyup = (e) => {
+  // e.preventDefault();
+  // console.log(e);
+  // nameTag.parentElement.classList.add("show-error");
+  // nameTag.style.borderColor = "red";
+  // nameTag.style.border = 1rem;
   isNameDirty = true;
 
   enableOrDisableButton();
+
+  const isInputOk = isNameTagValid(nameTag);
+
+  // if (!nameTag.parentElement.classList.contains("show-error")) {
+  //   nameTag.parentElement.classList.add("show-error");
+  //   nameTag.parentElement.bordercolor = "red";
+  // }
+
+  isInputOk ? removeErrorBorder(nameTag) : addErrorBorder(nameTag);
+  console.log(nameTag.classList);
 };
+
+// document.querySelector(".tooltip").onmouseover = () => {
+// nameTag.onmouseover = () => {
+//   console.log("Hover name");
+// };
 
 usernameTag.onkeyup = () => {
   isUsernameDirty = true;
 
   enableOrDisableButton();
+
+  const isInputOk = isNameTagValid(usernameTag);
+
+  isInputOk ? removeErrorBorder(usernameTag) : addErrorBorder(usernameTag);
 };
 
 emailTag.onkeyup = () => {
   isEmailDirty = true;
 
   enableOrDisableButton();
+
+  const isInputOk = isNameTagValid(emailTag);
+  isInputOk ? removeErrorBorder(emailTag) : addErrorBorder(emailTag);
 };
 
 passwordTag.onkeyup = () => {
   isPasswordDirty = true;
 
   enableOrDisableButton();
+
+  const isInputOk = isNameTagValid(passwordTag);
+  isInputOk ? removeErrorBorder(passwordTag) : addErrorBorder(passwordTag);
 };
 
 confirmPasswordTag.onkeyup = () => {
   isConfirmDirty = true;
 
   enableOrDisableButton();
+
+  const isInputOk = isNameTagValid(confirmTag);
+  isInputOk ? removeErrorBorder(confirmTag) : addErrorBorder(confirmTag);
 };
 
-const isNameTagValid = () => {
-  return nameTag.isDirty();
+const isNameTagValid = (tag) => {
+  const pattern = tag.getAttribute("pattern");
+  const re = new RegExp(pattern);
+  const value = tag.value;
+
+  return re.test(value);
+};
+
+const removeErrorBorder = (tag, isOk) => {
+  if (tag.classList.contains("show-error")) {
+    tag.classList.remove("show-error");
+  }
+};
+
+const addErrorBorder = (tag) => {
+  if (!tag.classList.contains("show-error")) {
+    tag.classList.add("show-error");
+  }
+};
+
+const isUsernameTagValid = () => {
+  return usernameTag.isDirty();
+};
+
+const isEmailTagValid = () => {
+  return emailTag.isDirty();
+};
+
+const isPasswordTagValid = () => {
+  return passwordTag.isDirty();
+};
+
+const isConfirmTagValid = () => {
+  return confirmTag.isDirty();
 };
 
 const enableOrDisableButton = () => {
