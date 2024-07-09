21ae6bc96ba7a41119b6b0b09e420f474affc038
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-07-09 12:46:39 +0200

    Cleanup and fix confirm password

--- index.js
+++ index.js
@@ -13,84 +13,28 @@ let isEmailDirty = false;
 let isPasswordDirty = false;
 let isConfirmDirty = false;
 
-nameTag.addEventListener("focusin", () => {
-  event.target.style.background = "lightgray";
-
-  removeErrorBorder(nameTag);
-  disableTooltip(nameTag);
-});
-
-nameTag.addEventListener("focusout", () => {
-  event.target.style.background = "";
-
-  if (!nameTag.checkValidity()) {
-    addErrorBorder(nameTag);
-    enableTooltip(nameTag);
-  }
-
-  isNameDirty = true;
-  enableOrDisableButton();
-});
-
-usernameTag.onkeyup = () => {
-  isUsernameDirty = true;
-
-  enableOrDisableButton();
-
-  const isInputOk = isNameTagValid(usernameTag);
-
-  isInputOk ? removeErrorBorder(usernameTag) : addErrorBorder(usernameTag);
-};
-
-emailTag.onkeyup = () => {
-  isEmailDirty = true;
-
-  enableOrDisableButton();
-
-  const isInputOk = isNameTagValid(emailTag);
-  isInputOk ? removeErrorBorder(emailTag) : addErrorBorder(emailTag);
-};
-
-passwordTag.onkeyup = () => {
-  isPasswordDirty = true;
-
-  enableOrDisableButton();
-
-  const isInputOk = isNameTagValid(passwordTag);
-  isInputOk ? removeErrorBorder(passwordTag) : addErrorBorder(passwordTag);
-};
-
-confirmPasswordTag.onkeyup = () => {
-  isConfirmDirty = true;
-
-  enableOrDisableButton();
-
-  const isInputOk = isNameTagValid(confirmTag);
-  isInputOk ? removeErrorBorder(confirmTag) : addErrorBorder(confirmTag);
-};
-
-const isNameTagValid = (tag) => {
-  const pattern = tag.getAttribute("pattern");
-  const re = new RegExp(pattern);
-  const value = tag.value;
-
-  return re.test(value);
-};
-
-const removeErrorBorder = (tag, isOk) => {
+const removeErrorBorder = (tag) => {
   if (tag.classList.contains("show-error")) {
     tag.classList.remove("show-error");
   }
 };
 
-const disableTooltip = (tag, isOk) => {
+const disableTooltip = (tag) => {
   const errorTag = tag.parentElement.querySelector("#name-error");
   if (errorTag.classList.contains("tooltip-show")) {
     errorTag.classList.remove("tooltip-show");
   }
-  // if (tag.classList.contains("tooltip-show")) {
-  //   tag.classList.remove("tooltip-show");
-  // }
+};
+
+const resetGUI = (tag) => {
+  removeErrorBorder(tag);
+  disableTooltip(tag);
+};
+
+const focusinFunc = () => {
+  event.target.style.background = "lightgray";
+
+  resetGUI(event.target);
 };
 
 const addErrorBorder = (tag) => {
@@ -104,43 +48,6 @@ const enableTooltip = (tag) => {
   if (!errorTag.classList.contains("tooltip-show")) {
     errorTag.classList.add("tooltip-show");
   }
-  // if (!tag.classList.contains("tooltip-show")) {
-  //   tag.classList.add("tooltip-show");
-  // }
-};
-
-const isUsernameTagValid = () => {
-  return usernameTag.isDirty();
-};
-
-const isEmailTagValid = () => {
-  return emailTag.isDirty();
-};
-
-const isPasswordTagValid = () => {
-  return passwordTag.isDirty();
-};
-
-const isConfirmTagValid = () => {
-  return confirmTag.isDirty();
-};
-
-const enableOrDisableButton = () => {
-  !isFormTouched() || isFormValid()
-    ? buttonTag.removeAttribute("disabled")
-    : buttonTag.setAttribute("disabled", "disabled");
-};
-
-const isFormValid = () => {
-  return isAllFormItemsValid() && isPasswordsEqual();
-};
-
-const isAllFormItemsValid = () => {
-  return formTag.checkValidity();
-};
-
-const isPasswordsEqual = () => {
-  return passwordTag.value === confirmPasswordTag.value;
 };
 
 const isFormTouched = () => {
@@ -153,12 +60,164 @@ const isFormTouched = () => {
   );
 };
 
+const isFormValid = () => {
+  return isAllFormItemsValid() && isPasswordsEqual();
+};
+
+const enableOrDisableButton = () => {
+  !isFormTouched() || isFormValid()
+    ? buttonTag.removeAttribute("disabled")
+    : buttonTag.setAttribute("disabled", "disabled");
+};
+
+const validiteTag = (tag) => {
+  return tag.checkValidity();
+};
+
+const validiteConfirmTag = (tag) => {
+  // If password is valid this must also be valid
+  return tag.value == confirmPasswordTag.value;
+};
+
+const handleGUI = (tag, validate) => {
+  if (!validate(tag)) {
+    addErrorBorder(tag);
+    enableTooltip(tag);
+  }
+  enableOrDisableButton();
+};
+
+const focusoutFunc = () => {
+  event.target.style.background = "";
+
+  handleGUI(event.target, validiteTag);
+
+  isNameDirty = true;
+};
+
+const isPasswordValid = () => {
+  // Check if password is correct, if not ignore this
+  return passwordTag.checkValidity();
+};
+
+nameTag.addEventListener("focusin", focusinFunc);
+nameTag.addEventListener("focusout", focusoutFunc);
+
+usernameTag.addEventListener("focusin", focusinFunc);
+usernameTag.addEventListener("focusout", focusoutFunc);
+
+emailTag.addEventListener("focusin", focusinFunc);
+emailTag.addEventListener("focusout", focusoutFunc);
+
+passwordTag.addEventListener("focusin", focusinFunc);
+passwordTag.addEventListener("focusout", focusoutFunc);
+
+confirmPasswordTag.addEventListener("focusin", focusinFunc);
+confirmPasswordTag.addEventListener("focusout", () => {
+  event.target.style.background = "";
+
+  // Check if password is correct, if not ignore this
+  if (!isPasswordValid()) return;
+
+  handleGUI(event.target, validiteConfirmTag);
+
+  isNameDirty = true;
+});
+
+// Enable submit button at startup
+enableOrDisableButton();
+
+// usernameTag.addEventListener("focusin", () => {
+//   event.target.style.background = "lightgray";
+
+//   removeErrorBorder(usernameTag);
+//   disableTooltip(usernameTag);
+// });
+
+// usernameTag.addEventListener("focusout", () => {
+//   event.target.style.background = "";
+
+//   if (!usernameTag.checkValidity()) {
+//     addErrorBorder(usernameTag);
+//     enableTooltip(usernameTag);
+//   }
+
+//   isNameDirty = true;
+//   enableOrDisableButton();
+// });
+
+// usernameTag.onkeyup = () => {
+//   isUsernameDirty = true;
+
+//   enableOrDisableButton();
+
+//   const isInputOk = isNameTagValid(usernameTag);
+
+//   isInputOk ? removeErrorBorder(usernameTag) : addErrorBorder(usernameTag);
+// };
+
+// emailTag.onkeyup = () => {
+//   isEmailDirty = true;
+
+//   enableOrDisableButton();
+
+//   const isInputOk = isNameTagValid(emailTag);
+//   isInputOk ? removeErrorBorder(emailTag) : addErrorBorder(emailTag);
+// };
+
+// passwordTag.onkeyup = () => {
+//   isPasswordDirty = true;
+
+//   enableOrDisableButton();
+
+//   const isInputOk = isNameTagValid(passwordTag);
+//   isInputOk ? removeErrorBorder(passwordTag) : addErrorBorder(passwordTag);
+// };
+
+// confirmPasswordTag.onkeyup = () => {
+//   isConfirmDirty = true;
+
+//   enableOrDisableButton();
+
+//   const isInputOk = isNameTagValid(confirmTag);
+//   isInputOk ? removeErrorBorder(confirmTag) : addErrorBorder(confirmTag);
+// };
+
+// const isNameTagValid = (tag) => {
+//   const pattern = tag.getAttribute("pattern");
+//   const re = new RegExp(pattern);
+//   const value = tag.value;
+
+//   return re.test(value);
+// };
+
+// const isUsernameTagValid = () => {
+//   return usernameTag.isDirty();
+// };
+
+// const isEmailTagValid = () => {
+//   return emailTag.isDirty();
+// };
+
+// const isPasswordTagValid = () => {
+//   return passwordTag.isDirty();
+// };
+
+// const isConfirmTagValid = () => {
+//   return confirmTag.isDirty();
+// };
+
+// const isAllFormItemsValid = () => {
+//   return formTag.checkValidity();
+// };
+
+// const isPasswordsEqual = () => {
+//   return passwordTag.value === confirmPasswordTag.value;
+// };
+
 const registrationData = {
   name: "first name last name",
   username: "username",
   email: "email@email.com",
   password: "password",
 };
-
-// Enable submit button at startup
-enableOrDisableButton();
